bab683569786bd0f3c887cd15077d59b8065719e
Felix Schäfer <felix@thegcat.net>
2015-01-25 23:40:52 +0100

    Refactor identifiers_controller_spec

--- spec/controllers/identifiers_controller_spec.rb
+++ spec/controllers/identifiers_controller_spec.rb
@@ -7,34 +7,29 @@ RSpec.describe IdentifiersController, type: :controller do
   before {allow(controller).to receive(:doorkeeper_token) {token}}
 
   describe '#show' do
-    context 'when the resource is a user' do
-      before {get :show, id: 'U1'}
+    let(:code) {'U1'}
+    before {Identifier.find_by_code('U1').try(:identifiable) || create(:user, code: 'U1')}
+    before {Identifier.find_by_code('P1').try(:identifiable) || create(:product, code: 'P1')}
 
-      context 'when the resource exists' do
-        before(:all) {Identifier.find_by_code('U1').try(:identifiable) || create(:user, code: 'U1')}
-        its(:content_type) {is_expected.to eq 'application/json'}
-        it {is_expected.to have_http_status(:ok)}
+    before {get :show, id: code}
 
-        context 'the JSON response' do
-          subject {JSON.parse(response.body)}
+    context 'when the resource exists' do
+      its(:content_type) {is_expected.to eq 'application/json'}
+      it {is_expected.to have_http_status(:ok)}
 
-          its(['type']) {is_expected.to eq 'user'}
+      describe 'the JSON response' do
+        context 'when the resource is a user' do
+          it 'should be of the type user' do
+            expect(JSON.parse(subject.body)['type']).to eq 'user'
+          end
         end
-      end
-    end
 
-    context 'when the resource is a product' do
-      before {get :show, id: 'P1'}
+        context 'when the resource is a product' do
+          let(:code) {'P1'}
 
-      context 'when the resource exists' do
-        before(:all) {Identifier.find_by_code('P1').try(:identifiable) || create(:product, code: 'P1')}
-        its(:content_type) {is_expected.to eq 'application/json'}
-        it {is_expected.to have_http_status(:ok)}
-
-        context 'the JSON response' do
-          subject {JSON.parse(response.body)}
-
-          its(['type']) {is_expected.to eq 'product'}
+          it 'should be of the type product' do
+            expect(JSON.parse(subject.body)['type']).to eq 'product'
+          end
         end
       end
     end
