7a851cb45848b30194c1d8d882e0817ecee3a0da
Felix Schäfer <felix@thegcat.net>
2015-02-02 23:15:47 +0100

    swagger-docs should touch my property name

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -17,6 +17,8 @@ Swagger::Docs::Config.register_apis({
     :base_path => "https://oskiosk.herokuapp.com",
     # if you want to delete all .json files at each generation
     :clean_directory => false,
+    # leave my property names as I write them ffs
+    :camelize_model_properties => false,
     # add custom attributes to api-docs
     :attributes => {
       :info => {
