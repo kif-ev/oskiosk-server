839e3fa6929492cf59317d1eb1716975ca6031f8
Felix Schäfer <felix@thegcat.net>
2015-04-24 21:02:44 +0200

    Add own parse_strategy for cart_items

--- app/representers/cart_representer.rb
+++ app/representers/cart_representer.rb
@@ -5,7 +5,18 @@ module CartRepresenter
   property :id, writeable: false
   property :user_id
 
-  collection :cart_items, extend: CartItemRepresenter, class: CartItem
+  collection(
+    :cart_items,
+    extend: CartItemRepresenter,
+    class: CartItem,
+    parse_strategy: lambda do |fragment, _, _|
+      cart_item = cart_items.where(pricing_id: fragment['pricing_id']).
+                  first_or_initialize
+      cart_item.quantity = fragment['quantity']
+      cart_items << cart_item
+      cart_item
+    end
+  )
 
   link :self do
     cart_path(self)
