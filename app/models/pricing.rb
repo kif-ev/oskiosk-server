153258d042a0aa8bf6c949c0b36e0f518240c727
Felix Schäfer <felix@thegcat.net>
2015-05-02 16:44:18 +0200

    Destroy CartItems alongside the cart

--- app/models/pricing.rb
+++ app/models/pricing.rb
@@ -1,6 +1,6 @@
 class Pricing < ActiveRecord::Base
   belongs_to :product
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
 
   def available_quantity
     quantity - cart_items.sum(:quantity)
