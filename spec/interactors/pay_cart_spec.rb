13a0a252796880a147948983dcd87d6731312acb
Felix Schäfer <felix@thegcat.net>
2015-05-03 19:06:30 +0200

    Indentation

--- spec/interactors/pay_cart_spec.rb
+++ spec/interactors/pay_cart_spec.rb
@@ -24,33 +24,34 @@ RSpec.describe PayCart do
     end
 
     context 'when everything is swell' do
-    it 'succeeds' do
-      interactor.call
-      expect(context).to be_a_success
-    end
+      it 'succeeds' do
+        interactor.call
+        expect(context).to be_a_success
+      end
 
-    it 'updates the first product\'s quantities' do
-      expect {interactor.call}.to change(pricings[0], :quantity).from(5).to(3)
-    end
-    it 'updates the second product\'s quantities' do
-      expect {interactor.call}.to change(pricings[1], :quantity).from(5).to(4)
-    end
+      it 'updates the first product\'s quantities' do
+        expect {interactor.call}.to change(pricings[0], :quantity).from(5).to(3)
+      end
+      it 'updates the second product\'s quantities' do
+        expect {interactor.call}.to change(pricings[1], :quantity).from(5).to(4)
+      end
 
-    it 'debits the user correctly' do
-      expect {interactor.call}.to change(user, :balance).from(1000).to(700)
-    end
+      it 'debits the user correctly' do
+        expect {interactor.call}.to change(user, :balance).from(1000).to(700)
+      end
 
-    it 'deletes the cart' do
-      expect(cart).to receive(:destroy)
-      interactor.call
-    end
+      it 'deletes the cart' do
+        expect(cart).to receive(:destroy!)
+        interactor.call
+      end
 
-    it 'deletes the cart_items' do
-      expect {interactor.call}.to change(CartItem, :count).by(-2)
-    end
+      it 'deletes the cart_items' do
+        expect {interactor.call}.to change(CartItem, :count).by(-2)
+      end
 
-    it 'creates a transaction' do
-      expect {interactor.call}.to change(Transaction, :count).by(1)
+      it 'creates a transaction' do
+        expect {interactor.call}.to change(Transaction, :count).by(1)
+      end
     end
     end
   end
