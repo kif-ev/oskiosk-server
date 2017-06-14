477fec97be0a8902c6963c8ea1095ad5b675003e
Felix Schäfer <felix@thegcat.net>
2017-06-14 11:43:14 +0200

    Nicer syntax in routes definition

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   use_doorkeeper
   get 'heartbeat' => 'heartbeat#check'
-  resources :identifiers, only: [:show]
-  resources :users, only: [:show, :index, :create, :update] do
-    resource :user_deposit, only: [:create], path: 'deposit'
+  resources :identifiers, only: :show
+  resources :users, only: %i(show index create update) do
+    resource :user_deposit, only: :create, path: 'deposit'
   end
-  resources :products, only: [:index, :show, :create, :update]
-  resources :carts, only: [:create, :show, :update] do
-    resource :cart_payment, only: [:create], path: 'pay'
+  resources :products, only: %i(index show create update)
+  resources :carts, only: %i(create show update) do
+    resource :cart_payment, only: :create, path: 'pay'
   end
-  resources :transactions, only: [:index, :show] do
+  resources :transactions, only: %i(index show) do
     collection do
-      match 'search', via: [:post]
+      match 'search', via: :post
     end
   end
 end
